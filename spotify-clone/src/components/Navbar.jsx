7b1c1e1efa1566ebbe9785998765d1700c16b594
muthuhariharan2001 <164756456+muthuhariharan2001@users.noreply.github.com>
2024-09-24 15:11:10 +0530

    Update Navbar.jsx

--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 </div>
                 <div className="flex items-center gap-4">
                     <p className="bg-white text-black text-[15px] cursor-pointer px-4 py-1 rounded-2xl hidden md:block">Explore Premium</p>
-                    <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
+                    <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer hidden md:block">Install App</p>
                     <p className="bg-purple-500 text-black w-7 h-7 items-center justify-center flex rounded-full">G</p>
                 </div>
             </div>
